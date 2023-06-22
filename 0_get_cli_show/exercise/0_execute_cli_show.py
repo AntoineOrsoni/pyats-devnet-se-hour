ca6f234ef3003922757a915ce31efb137d27be99
Romain <romain.cyrille84@gmail.com>
2023-06-22 09:43:09 +0200

    Update 0_execute_cli_show.py
    Typo in hostname

--- 0_get_cli_show/exercise/0_execute_cli_show.py
+++ 0_get_cli_show/exercise/0_execute_cli_show.py
@@ -2,7 +2,7 @@ from pyats.topology import loader
 
 # Step 0: Load the testbed
 
-# Step 1: testbed.devices is a dictionary. Extract the device Cat8000v
+# Step 1: testbed.devices is a dictionary. Extract the device Cat8000V
 
 # Step 2: Connect to the device
 
